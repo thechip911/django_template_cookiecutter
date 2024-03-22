764bc38e151e37222b7fc9d26bf543a854b49586
payme-alok <thechip911@gmail.com>
2024-03-22 20:43:53 +0530

    ADDED: Updated For Project Slug

--- {{ cookiecutter.project_slug }}/infra_django_template/urls.py
+++ {{ cookiecutter.project_slug }}/infra_django_template/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for infra_django_template project.
+URL configuration for {{ cookiecutter.project_slug }} project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
